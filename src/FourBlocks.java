f5db7e536630fa827cab8a9ed66e82bdd09d30ef
FluxDenisty <ryanbateman.uw@gmail.com>
2010-12-02 00:12:07 -0500

    Cleaned up old code and changed dimentions to be a little nicer. Also fixed mini bugs

--- src/FourBlocks.java
+++ src/FourBlocks.java
@@ -12,7 +12,6 @@ import javax.swing.*;
 public class FourBlocks extends JFrame{
 	
 	public static BlockLib lib;
-	int score;
 	JLabel header;
 	JButton start;
 	JPanel all;
@@ -24,44 +23,35 @@ public class FourBlocks extends JFrame{
 	JLabel scoreDisplay;
 	JLabel game;
 	Block active;
-	Color bg;
 	Timer gameClock;
-	Boolean canMove;
 	static Timer refresh;
-	int delay;
-	static int refreshRate;
 	Block.blockType[][] grid;
-	static FourBlocks instance;
-	Boolean running;
-	Boolean candrop;
+	Color bg = Color.GRAY;
+	int score = 0;
+	int delay = 1000;
+	Boolean canMove = true;
+	Boolean running = false;
+	Boolean candrop = true;
 	
 	public static void main(String[] args) {
 		lib = new BlockLib();
 		final FourBlocks main = new FourBlocks();
-//		refresh = new Timer(refreshRate, new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent arg0) {
-//				instance = main;
-//			}
-//		});
 	}
 	
 	FourBlocks() {
-		running = false;
-		canMove = true;
-		setVisible(false);
-		refreshRate = 100;
-		score = 0;
 		
-		grid = new Block.blockType[10][20];
+		setVisible(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400, 600);
+		
+		grid = new Block.blockType[10][20]; // init game grid to null (empty square) 
 		for (int i = 0; i < 10; i++){
 			for (int j = 0; j < 20; j++){
 				grid[i][j] = null;
 			}
 		}
 		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(400, 600);
+		//Listeners
 		KeyListener actions = new KeyListener() {
 			
 			@Override
@@ -79,8 +69,8 @@ public class FourBlocks extends JFrame{
 			}
 			
 			@Override
-			public void keyPressed(KeyEvent arg0) {
-				if (canMove && running){
+			public void keyPressed(KeyEvent arg0) {  
+				if (canMove && running){  //Take ACTION
 					canMove = false;
 					if (arg0.getKeyCode() == KeyEvent.VK_DOWN){
 						down();
@@ -93,48 +83,30 @@ public class FourBlocks extends JFrame{
 					}else if (arg0.getKeyCode() == KeyEvent.VK_SPACE){
 							fall();
 					}
-//					paintBlock(blockBoard.getGraphics(), active);
+					display(blockBoard.getGraphics());
 					canMove = true;
-				}else if (!running && arg0.getKeyCode() == KeyEvent.VK_ENTER){
-//					header.setText("BEGIN");
+				}else if (!running && arg0.getKeyCode() == KeyEvent.VK_ENTER){ //Begin new game
 					game.setVisible(false);
 					clearBoard();
 					active = new Block();
-					display(blockBoard.getGraphics());
-//					paintBlock(blockBoard.getGraphics(), active);
 					gameClock.start();
 					running = true;
-				}
-			}
-		};
-		ActionListener go = new ActionListener() {
-		
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (e.getActionCommand().equals("Start") && !running){
-					header.setText("BEGIN");
-					active = new Block();
-					display(blockBoard.getGraphics());
-					gameClock.start();
+					canMove = true;
 					candrop = true;
-					blockBoard.remove(game);
-					running = true;
+					display(blockBoard.getGraphics());
 				}
 			}
 		};
-		//top area
-		header = new JLabel("I HAS A JAVA", SwingConstants.CENTER);
-		header.setLocation(0, 0);
 		
-		start = new JButton("Start");
-		start.addActionListener(go);
+		gameClock = new Timer(delay, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (canMove)
+					down();
+			}
+		});
 		
-		topPanel = new JPanel();
-		topPanel.setAlignmentX(CENTER_ALIGNMENT);
-		topPanel.add(header);
-		topPanel.add(start);
-		
-		//game panel
+		//GUI init
 		game = new JLabel();
 		game.setPreferredSize(new Dimension(200, 100));
 		game.setAlignmentX(CENTER_ALIGNMENT);
@@ -144,7 +116,6 @@ public class FourBlocks extends JFrame{
 		blockBoard = new JPanel();
 		blockBoard.setAlignmentY(CENTER_ALIGNMENT);
 		blockBoard.setAlignmentX(CENTER_ALIGNMENT);
-		bg = Color.GRAY;
 		blockBoard.setBackground(bg);
 		blockBoard.setPreferredSize(new Dimension(251, 501));
 		blockBoard.add(game);
@@ -157,33 +128,21 @@ public class FourBlocks extends JFrame{
 		scoreBox.add(scoreDisplay);
 		
 		info = new JPanel();
-		info.setPreferredSize(new Dimension(100, 500));
+		info.setPreferredSize(new Dimension(100, this.getHeight()));
 		info.setBackground(new Color(102, 204, 204));
 		info.add(scoreBox);
 		
 		gameBoard = new JPanel();
-		gameBoard.setPreferredSize(new Dimension(400, 550));
+		gameBoard.setPreferredSize(new Dimension(400, this.getHeight()));
 		gameBoard.setBackground(bg);
 		gameBoard.add(blockBoard);
 		gameBoard.add(info);
 
 		all = new JPanel();
-		all.setPreferredSize(new Dimension(400, 600));
+		all.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()));
 		all.setAlignmentX(LEFT_ALIGNMENT);
-		
-//		all.add(topPanel);
 		all.add(gameBoard);
 		all.addKeyListener(actions);
-		
-		delay = 1000;
-		gameClock = new Timer(delay, new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				if (canMove)
-					down();
-			}
-		});
-
 
 		this.add(all);
 		
@@ -258,48 +217,28 @@ public class FourBlocks extends JFrame{
 		g.setColor(old);
 	}
 	
-//	public void clearBlock(Graphics g,Block b){
-//		g.setColor(bg);
-//		for (int i = 0; i < b.sizex; i++){
-//			for (int j = 0; j < b.sizey; j++){
-//				if (b.filled[j][i])
-//					clearSquare(b.x + i, b.y + j, g);
-//			}
-//		}
-//	}
-//	public void clearSquare(int x, int y, Graphics g){
-//		g.fillRect(x * 25,y * 25,25,25);
-//	}
-	
 	public void fall(){
 		while (checkColision(0,1)){
 			active.y++;
 			if (!running)
 				gameOver();
 		}
-		display(blockBoard.getGraphics());
 	}
 	public void down(){
 		if (checkColision(0,1)){
 			active.y++;
 		}
-		display(blockBoard.getGraphics());
-//		paintBlock(blockBoard.getGraphics(), active);
 		if (!running)
 			gameOver();
 	}
 	public void left(){
 		if (checkColision(-1,0)){
 			active.x--;
-			display(blockBoard.getGraphics());
-//			paintBlock(blockBoard.getGraphics(), active);
 		}
 	}
 	public void right(){
 		if (checkColision(1,0)){
-			active.x++;
-			display(blockBoard.getGraphics());
-//			paintBlock(blockBoard.getGraphics(), active);	
+			active.x++;	
 		}
 	}
 	public void rotate(){
@@ -312,36 +251,9 @@ public class FourBlocks extends JFrame{
 		active.filled = active.model[next];
 		if (checkColision(0, 0)){
 			active.dir = next;
-			display(blockBoard.getGraphics());
 		}else{
 			active.filled = active.model[old];
 		}
-		
-		
-		/** OLD METHOD **
-		Boolean[][] temp = new Boolean[active.sizex][active.sizey];
-		for (int i = 0; i < active.sizex; i++){
-			for (int j = 0; j < active.sizey; j++){
-				temp[i][j] = active.filled[j][i];
-			}
-		}
-		for (int i = 0; i < active.sizey; i++){
-			for (int j = 0; j < active.sizex; j++){
-				if (temp[j][i]){
-					try {
-						if (grid[i + active.x][j + active.y] != null)
-							return;
-					}catch(Exception e){
-							return;
-					}
-				}
-			}
-		}
-		active.filled = temp;
-		int x = active.sizex;
-		active.sizex = active.sizey;
-		active.sizey = x;
-		display(blockBoard.getGraphics()); **/
 	}
 	public Boolean checkColision(int x,int y){
 		for (int i = 0; i < active.sizex; i++){
@@ -380,7 +292,6 @@ public class FourBlocks extends JFrame{
 			}
 		}
 		checkRows();
-		display(blockBoard.getGraphics());
 		scoreDisplay.setText(Integer.toString(score));
 		active = new Block();
 			
@@ -411,6 +322,7 @@ public class FourBlocks extends JFrame{
 		gameClock.stop();
 		running = false;
 		canMove = false;
+		display(blockBoard.getGraphics());
 		game.setText("Game Over!");
 		game.setVisible(true);
 	}
